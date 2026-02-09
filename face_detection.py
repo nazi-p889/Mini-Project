d3ec4c94ac9981148020d6c004b7ce872b7d45c5
nazi-p889 <parveennaziya921@gmail.com>
2026-02-09 11:21:22 +0530

    Implement image upload and emotion analysis
    Added functionality to upload and analyze an image for emotion detection.

--- face_detection.py
+++ face_detection.py
@@ -31,3 +31,25 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+Emotion detection
+
+from deepface import DeepFace
+import cv2
+from google.colab import files
+
+# Upload image
+uploaded = files.upload()
+img_path = list(uploaded.keys())[0]
+
+# Read image
+img = cv2.imread(img_path)
+
+# Analyze emotion
+result = DeepFace.analyze(
+    img,
+    actions=['emotion'],
+    enforce_detection=False
+)
+
+print("Dominant Emotion:", result[0]['dominant_emotion'])
